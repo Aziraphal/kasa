9fd06a9363c413f30bcbe54bf4510d5e5523ac39
Cyril Paquier <cyril.paquier@gmail.com>
2023-02-15 10:53:03 +0100

    header et bannière

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,9 +1,15 @@
 import React from 'react';
-
+import './banner.css';
+import Img from '../../assets/IMG.png';
 const Banner = () => {
     return (
-        <div>
-            Banner
+        <div className='HomeBanner'>
+            <img src={Img} alt="côte, falaise" className='KasaBanner'></img>
+            <div className='BannerText'>
+            <h1>
+                Chez vous, partout et ailleurs
+            </h1>
+        </div>
         </div>
     );
 };
