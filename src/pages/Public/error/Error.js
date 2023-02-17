1177cf4a3ef43a62dedd98d7235f5be9f935e173
Cyril Paquier <cyril.paquier@gmail.com>
2023-02-17 16:54:50 +0100

    fin page erreur

--- src/pages/Public/error/Error.js
+++ src/pages/Public/error/Error.js
@@ -2,14 +2,16 @@ import React from 'react';
 import './error.css';
 import Header from '../../../components/header/Header';
 import { Link } from 'react-router-dom';
+import Footer from '../../../components/footer/Footer';
 const Error = () => {
     return (
         <div className='error'>
             <Header />
             <div className='errorInfo'>
             <h1 className='errorTitle'>404</h1>
-            <p className='errorContent'>Oups! La page que vous demandé n'existe pas.</p>
+            <p className='errorContent'>Oups! La page que vous demandez n'existe pas.</p>
             <Link className='errorReturn' to='/'>Retourner sur la page d'accueil</Link>
+            <Footer />
             </div>
         </div>
     );
