f9d47a918f8f26bdcc1202f3f060c2b31558f79c
Cyril Paquier <cyril.paquier@gmail.com>
2023-02-23 19:02:05 +0100

    page hébergement presque terminée

--- src/pages/Public/hebergement/Hebergement.js
+++ src/pages/Public/hebergement/Hebergement.js
@@ -6,39 +6,66 @@ import Header from "../../../components/header/Header";
 import SlideShow from "../../../components/carrousel/Carrousel";
 import Collapse from "../../../components/collapse/Collapse";
 import Footer from "../../../components/footer/Footer";
-
-
+import greyStar from '../../../assets/grey_star.png';
+import redStar from '../../../assets/red_star.png';
 export default function Hebergement() {
 const [imageSlider, setImageSlider] = useState([]);
-
-	const idHebergement = useParams('id').id;
-	const dataHbebergementActuel = data.filter(data => data.id === idHebergement);
-
+    const idHebergement = useParams('id').id;
+    const dataHebergementActuel = data.filter(data => data.id === idHebergement);
     useEffect(() => {
-		const dataHbebergementActuel = data.filter(data => data.id === idHebergement);
-		setImageSlider(dataHbebergementActuel[0].pictures);
-	}, [idHebergement]);
-
+        const dataHebergementActuel = data.filter(data => data.id === idHebergement);
+        setImageSlider(dataHebergementActuel[0].pictures);
+    }, [idHebergement]);
+    const name = dataHebergementActuel[0].host.name.split(' '); 
+    const rating = dataHebergementActuel[0].rating;
+    const description  = dataHebergementActuel[0].description;
+    const equipments = dataHebergementActuel[0].equipments;
     return (
         <div>
             <Header />
             <SlideShow imageSlider={imageSlider} />
             <main className="hebergement">
-				<div className="contenuHebergement">
-					<div>
-						<h1>{dataHbebergementActuel[0].title}</h1>
-						<p>{dataHbebergementActuel[0].location}</p>
-						<div>
-							{dataHbebergementActuel[0].tags.map((tag, index) => {
-								return (
-									<button key={index}>{tag}</button>
-								)
-							})}
-						</div>
-					</div>
-				</div>    
-			</main>
-			<Footer/>
-		</div>
-	)
+                <div className="contenuHebergement">
+                    <div className="infoHebergement">
+                        <h1>{dataHebergementActuel[0].title}</h1>
+                        <p>{dataHebergementActuel[0].location}</p>
+                        <div>
+                            {dataHebergementActuel[0].tags.map((tag, index) => {
+                                return (
+                                    <button key={index}>{tag}</button>
+                                )
+                            })}
+                        </div>
+                    </div>
+                    <div className="hoteHebergement">
+                        <div>
+                            <div className='infoHoteHebergement'>
+                                <span>{name[0]}</span>
+                                <span>{name[1]}</span>
+                            </div>
+                            <img src={dataHebergementActuel[0].host.picture} alt="Hôte de l'hébergement" />
+                        </div>
+                            
+                        <div className="hoteHebergementNote">
+                            {[...Array(5)].map((star, index) => {
+                                const ratingValue = index + 1;
+                                return (
+                                    <img key={index} src={ratingValue <= rating ? redStar : greyStar} alt="star" />
+                                )
+                            })}
+                        </div>
+                    </div>
+                </div>
+                <div className="hebergementCollapse">
+                    <div className="hebergementCollapseItem">
+                        <Collapse title={'Description'} content={description} />    
+                    </div>
+                    <div className="hebergementCollapseItem">
+                        <Collapse title={'Équipements'} content={equipments}/>
+                    </div>  
+                </div>
+            </main>
+            <Footer/>
+        </div>
+    )
 }
