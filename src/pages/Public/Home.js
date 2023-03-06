93d35f8472d06a050727bee54805857caaa43ff8
Cyril Paquier <cyril.paquier@gmail.com>
2023-03-06 06:50:01 +0100

    test correction header

--- src/pages/Public/Home.js
+++ src/pages/Public/Home.js
@@ -1,4 +1,4 @@
-import Header from '../../components/header/header';
+import Header from '../../components/header/Header';
 import Banner from '../../components/banner/Banner';
 import Gallery from '../../components/gallery/Gallery';
 import Footer from '../../components/footer/Footer';
